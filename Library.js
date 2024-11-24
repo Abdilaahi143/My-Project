d5e98b97dbba6db6f4a6c895915c05d0440956fc
Abdilaahi143 <Abdilaahi117@gmail.com>
2024-11-24 13:38:32 +0300

    Describe your changes

--- Library.js
+++ Library.js
@@ -35,36 +35,40 @@ function saveLibraryData(library) {
   
 
 
-function mainMenu(){
-    console.log("");
-    console.log("1.Add Books");
-    console.log("2.View Books");
-    console.log("3.Update Books");
-    console.log("4.Remove Books");
-    console.log("5.Search Books");
-    rl.question("Choose an option: ", (option)=>{
-        if(option.toLowerCase() == 1){
-            addBooks();
-        }
-        else if(option.toLowerCase() == 2){
-            viewBooks();
-        }
-        else if(option.toLowerCase == 3){
-            updateBooks();
-        }
-        else if(option.toLowerCase() == 4){
-            removeBooks();
-        }
-        else if(option.toLowerCase() == 5){
-            searchBooks();
-        }
-        else{
-            console.log("invalid Input! Please try again:");
-            console.logs();
-            mainMenu();
-        }
+function mainMenu() {
+    console.log("\nLibrary Management System:");
+    console.log("1. Add Books");
+    console.log("2. View Books");
+    console.log("3. Update Books");
+    console.log("4. Remove Books");
+    console.log("5. Search Books");
+    console.log("6. Exit");
+    rl.question("Choose an option: ", (option) => {
+      switch (option) {
+        case "1":
+          addBooks();
+          break;
+        case "2":
+          viewBooks();
+          break;
+        case "3":
+          updateBooks();
+          break;
+        case "4":
+          removeBooks();
+          break;
+        case "5":
+          searchBooks();
+          break;
+        case "6":
+          console.log("Exiting the Library System. Goodbye!");
+          rl.close();
+          break;
+        default:
+          console.log("Invalid input! Please try again.");
+          mainMenu();
+      }
     });
-
 }
 
 function addBooks(){
@@ -86,9 +90,20 @@ function addBooks(){
     });
 }
 
-function viewBooks(library){
-    console.log("Library contents:", library);
-
+function viewBooks() {
+    const library = loadLibraryData(); // Load data directly here
+    if (library.length === 0) {
+      console.log("No books in the library.");
+    } else {
+      console.log("\nLibrary contents:");
+      library.forEach((book, index) => {
+        console.log(`\nBook ${index + 1}:`);
+        console.log(`Title: ${book.title}`);
+        console.log(`Author: ${book.author}`);
+        console.log(`Category: ${book.category}`);
+        mainMenu();
+      });
+    }
 }
 
 
